f7f2c5ffd6d08983954ccf2346beb4e1bee60ee6
Bernard <bernardicheel@gmail.com>
2026-04-16 15:05:26 +0200

    backend

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Post(':param')
+  create(@Param('param') parametre: string) {
+    //return "cc";
+    return this.appService.create(parametre);
+  }
+}
+ 
